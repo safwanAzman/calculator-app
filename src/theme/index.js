5bc5cb54763cd9ca861d461b87db2117a7ea256a
safwan <safwanazman017@gmail.com>
2023-01-22 15:45:17 +0800

    feat: add theme config

---
+++ src/theme/index.js
@@ -0,0 +1,42 @@
+export const themeColor = {
+    theme1:{
+        main: 'bg-bgTheme1-mainBg',
+        screenBg: 'bg-bgTheme1-screenBg',
+        text1:'text-textTheme1-text1',
+        text3: 'text-textTheme1-text3',
+        keypadBg: 'bg-bgTheme1-keypadBg',
+        keyBg1: ' bg-keysTheme1-keyBg1',
+        keyBg2: 'bg-keysTheme1-keyBg2',
+        keyShadow1: 'shadow-keysTheme1-keyShadow1',
+        keyShadow2: 'shadow-keysTheme1-keyShadow2',
+        keyShadow3: 'shadow-keysTheme1-keyShadow3',
+        keyToggleBg1: 'bg-keysTheme1-keyToggleBg1'
+    },
+    theme2:{
+        main: 'bg-bgTheme2-mainBg',
+        screenBg: 'bg-bgTheme2-screenBg',
+        text1:'text-textTheme2-text1',
+        text3: 'text-textTheme2-text3',
+        keypadBg: 'bg-bgTheme2-keypadBg',
+        keyBg1: ' bg-keysTheme2-keyBg1',
+        keyBg2: 'bg-keysTheme2-keyBg2',
+        keyShadow1: 'shadow-keysTheme2-keyShadow1',
+        keyShadow2: 'shadow-keysTheme2-keyShadow2',
+        keyShadow3: 'shadow-keysTheme2-keyShadow3',
+        keyToggleBg1: 'bg-keysTheme2-keyToggleBg1'
+    },
+    theme3:{
+        main: 'bg-bgTheme3-mainBg',
+        screenBg: 'bg-bgTheme3-screenBg',
+        text1:'text-textTheme3-text1',
+        text3: 'text-textTheme3-text3',
+        keypadBg: 'bg-bgTheme3-keypadBg',
+        keyBg1: ' bg-keysTheme3-keyBg1',
+        keyBg2: 'bg-keysTheme3-keyBg2',
+        keyShadow1: 'shadow-keysTheme3-keyShadow1',
+        keyShadow2: 'shadow-keysTheme3-keyShadow2',
+        keyShadow3: 'shadow-keysTheme3-keyShadow3',
+        keyToggleBg1: 'bg-keysTheme3-keyToggleBg1'
+    },
+    
+}
